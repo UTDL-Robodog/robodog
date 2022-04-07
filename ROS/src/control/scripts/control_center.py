b3eed8ce02b5dff7f4975096853016a930385ee5
Ketan Vasudeva <ketanvasudeva@gmail.com>
2022-04-06 23:16:13 -0400

    added motor state json schema and updating logic

--- ROS/src/control/scripts/control_center.py
+++ ROS/src/control/scripts/control_center.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env
 import rospy
 from std_msgs.msg import String
+import json
+import os
 
 VALID_COMMANDS = ['off', 'walk', 'grab', 'return', 'standby']
+MOTOR_SCHEMA = "../../../other/motors.json"
 
 class RobotControl:
     def __init__(self):
-        self.motor_states = dict() # object to hold motor states
+
+        wd = os.path.dirname(__file__)
+        with open(os.path.join(wd,MOTOR_SCHEMA)) as motor_json:
+            self.motor_states = json.load(motor_json) # object to hold motor states
+
         self.mode = 'off' # current operating mode - Modes: off, walk, grab, return, standby
 
         rospy.init_node('control_center', anonymous=True)
 
         self.pub_motor_commands = rospy.Publisher('motor_commands', String, queue_size=10)
         rospy.Subscriber('mode_commands', String, self.update_mode)
+        rospy.Subscriber('motor_feedback', String, self.update_motors)
 
-    def update_mode(self, data):
+    def update_mode(self, data): # update the operating mode
         if data.data in VALID_COMMANDS:
             self.mode = data.data
             rospy.loginfo('Operating mode changed to: ' + str(data.data))
         else:
             rospy.logwarn(str(data.data) + ' is not a valid operating mode')
+    
+    def update_motors(self, data): # update the motor states
+        data_json = json.loads(data.data)
+        if self.motor_states.keys() == data_json.keys() and self.motor_states != data_json: # verify that json keys are valid (same number of motors)
+            self.motor_states = data_json
+            rospy.loginfo('Motor angles updated')
+        elif self.motor_states == data_json: # if format is valid but motor angles haven't changed, do nothing
+            pass
+        else: # if motor update is invalid
+            rospy.logwarn('Invalid motor angle update')
 
-    def run(self):
+
+    def run(self): # run the node's publisher
         r = rospy.Rate(1)
         while not rospy.is_shutdown():
 
@@ -39,6 +58,7 @@ class RobotControl:
                 pass
             
             print(self.mode)
+            print(self.motor_states["1"]["angle"])
             r.sleep()
 
 if __name__=='__main__':
